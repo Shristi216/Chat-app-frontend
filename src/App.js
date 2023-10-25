23f4b7fb72ea7f88f2748a70ef80db8a3ef7b925
Shristi216 <guptass_7@rknec.edu>
2023-10-25 10:53:23 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import Homepage from "./Pages/Homepage";
+import { Route } from "react-router-dom";
+import ChatPage from "./Pages/ChatPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route path="/" component={Homepage} exact />
+      <Route path="/chats" component={ChatPage} />
     </div>
   );
 }
 
 export default App;
+
+//ChatApp
+//CPqe14OK8fGz3YI2
